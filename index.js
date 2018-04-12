51d777460e592808755fff9e29ab71ed841a8887
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-04-12 21:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,13 +18,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string==string.toLowerCase())
   {
-    if(string==="I love you, Grandma")
-    return "I love you, too"
-    else
+    
     return "I can't hear you!";
   }
-  
-  else
+  else if(string=="I love you, Grandma")
   return "YES INDEED!"
   
 }
