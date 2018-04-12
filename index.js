55652774798de258b22d80943bd89efe5b6cec05
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-04-12 21:16:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   }
   else if(string=="I love you, Grandma")
+  return "I love you, too."
+  else if(string==string.toUpperCase())
   return "YES INDEED!"
   
 }
